b31d469f20405dbd05735c55ada1d4c52e9a1e51
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-06-24 21:57:35 +0530

    custom user class

--- user_control/models.py
+++ user_control/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 class CustomUserManager(BaseUserManager):
     def _create_user(self,username,password,**extra_fields):
@@ -9,11 +9,6 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save()
         return user
-    
-    def create_user(self,username,password,**extra_fields):
-        extra_fields.setdefault('is_staff',False)
-        extra_fields.setdefault('is_superuser',False)
-        return self._create_user(username,password,**extra_fields)
     
     def create_superuser(self,username,password,**extra_fields):
         extra_fields.setdefault('is_staff',True)
@@ -26,3 +21,14 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('Superuser must have is_superuser=True')
         
         return self._create_user(username,password,**extra_fields)
+
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    username = models.CharField(unique=True,max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'username'
+    objects = CustomUserManager()
