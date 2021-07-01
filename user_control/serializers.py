361fdf79deb81748d3ec92f1132c1032e9a09e83
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-07-01 19:01:15 +0530

    user profile serializer

--- user_control/serializers.py
+++ user_control/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from .models import UserProfile, CustomUser
+from message_control.serializers import GenericFileUploadSerializer
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
@@ -10,3 +12,19 @@ class RegisterSerializer(serializers.Serializer):
 
 class RefreshSerializer(serializers.Serializer):
     refresh = serializers.CharField()
+
+class CustomUserSerializer(serializers.Serializer):
+
+    class Meta:
+        model = CustomUser
+        fields = "__all__"
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer(read_only=True)
+    user_id = serializers.IntegerField(write_only=True)
+    profile_picture = GenericFileUploadSerializer(read_only=True)
+    profile_picture_id = serializers.IntegerField(write_only=True, required=False)
+
+    class Meta:
+        model = UserProfile
+        fields = "__all__"
