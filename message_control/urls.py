2d11f48d0d724ddf35956d18a8d2047ac6433fb9
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-07-03 02:05:07 +0530

    message url

--- message_control/urls.py
+++ message_control/urls.py
@@ -1,11 +1,12 @@
 from rest_framework import urlpatterns
 from rest_framework.routers import DefaultRouter
-from .views import GenericFileUploadView
+from .views import GenericFileUploadView, MessageView
 from django.urls import path, include
 
 router = DefaultRouter(trailing_slash = False)
 
 router.register("file-upload", GenericFileUploadView)
+router.register('message', MessageView)
 
 urlpatterns = [
     path("", include(router.urls))
