1e2db654f4895568586ab6ea01f0b3a21cac9c9a
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-07-03 00:25:32 +0530

    message serializers

--- message_control/serializers.py
+++ message_control/serializers.py
@@ -1,8 +1,31 @@
-from rest_framework.serializers import ModelSerializer
-from .models import GenericFileUpload
+from django.db import models
+from django.db.models import fields
+from rest_framework import serializers
+from .models import GenericFileUpload, Message, MessageAttachment
 
-class GenericFileUploadSerializer(ModelSerializer):
+class GenericFileUploadSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = GenericFileUpload
         fields = "__all__"
+
+class MessageAttachmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        models = MessageAttachment
+        fields = "__all__"
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = serializers.SerializerMethodField('get_user_data')
+    sender_id = serializers.IntegerField(write_only=True)
+    receiver = serializers.SerializerMethodField('get_user_data')
+    receiver_id = serializers.IntegerField(write_only=True)
+    message_attachments = MessageAttachmentSerializer(read_only=True, many=True)
+
+    class Meta:
+        models = Message
+        fields = "__all__"
+    
+    def get_user_data(self,obj):
+        from user_control.serializers import UserProfileSerializer
+        return UserProfileSerializer(obj.sender.user_profile)
